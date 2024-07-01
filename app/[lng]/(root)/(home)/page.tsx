5f23741b023971c0fb862197b6d50e4292dd2587
egsir1 <egsir111@gmail.com>
2024-07-02 00:11:11 +0900

    feat: Mobile & Hero section

--- app/[lng]/(root)/(home)/page.tsx
+++ app/[lng]/(root)/(home)/page.tsx
@@ -2,12 +2,17 @@
 
 import { useTranslation } from '@/i18n/client';
 import { useParams } from 'next/navigation';
+import Hero from './_components/hero';
 
 function Page() {
 	const { lng } = useParams();
 	const { t } = useTranslation(lng as string);
 
-	return <div className='mt-24'>{t('home')}</div>;
+	return (
+		<div>
+			<Hero />
+		</div>
+	);
 }
 
 export default Page;
